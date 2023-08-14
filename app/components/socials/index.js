e4e71228dbf8f4e6ccd6e326086f64bc6bc96fb2
Modestly Jen <jen@jenniferluchka.com>
2023-08-14 11:44:28 -0400

    Bug Fixes

--- app/components/socials/index.js
+++ app/components/socials/index.js
@@ -2,40 +2,38 @@ import React from "react";
 import { SocialIcon } from "react-social-icons";
 import "../../styles/Home.module.css";
 
-export default function Socials() {
-  return (
-    <div className="social">
-      <SocialIcon
-        className="socials"
-        url="https://twitter.com/theshamingofjay"
-        network="twitter"
-        bgColor="white"
-        fgColor="#ff3131"
-      />
+export const Socials = () => (
+  <div className="social">
+    <SocialIcon
+      className="socials"
+      url="https://twitter.com/theshamingofjay"
+      network="twitter"
+      bgColor="white"
+      fgColor="#ff3131"
+    />
 
-      <SocialIcon
-        className="socials"
-        url="https://www.tiktok.com/@theshamingofjay"
-        network="tiktok"
-        bgColor="white"
-        fgColor="#ff3131"
-      />
+    <SocialIcon
+      className="socials"
+      url="https://www.tiktok.com/@theshamingofjay"
+      network="tiktok"
+      bgColor="white"
+      fgColor="#ff3131"
+    />
 
-      <SocialIcon
-        className="socials"
-        url="https://www.instagram.com/theshamingofjay"
-        network="instagram"
-        bgColor="white"
-        fgColor="#ff3131"
-      />
+    <SocialIcon
+      className="socials"
+      url="https://www.instagram.com/theshamingofjay"
+      network="instagram"
+      bgColor="white"
+      fgColor="#ff3131"
+    />
 
-      <SocialIcon
-        className="socials"
-        url="https://www.facebook.com/profile.php?id=100091070141123&mibextid=LQQJ4d"
-        network="facebook"
-        bgColor="white"
-        fgColor="#ff3131"
-      />
-    </div>
-  );
-}
+    <SocialIcon
+      className="socials"
+      url="https://www.facebook.com/profile.php?id=100091070141123&mibextid=LQQJ4d"
+      network="facebook"
+      bgColor="white"
+      fgColor="#ff3131"
+    />
+  </div>
+);
